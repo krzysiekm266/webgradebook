5c8619622de993e693911d2607f5a6f6beb6447c
krzysiek <k@m.pl>
2022-11-16 06:01:43 +0100

    student updates

--- src/main/java/com/krzysiekm266/webgradebook/student/StudentController.java
+++ src/main/java/com/krzysiekm266/webgradebook/student/StudentController.java
@@ -15,35 +15,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 
 @RestController
-@RequestMapping(path = "/student")
+@RequiredArgsConstructor
 public class StudentController {
-    @Autowired
     StudentService studentService;
 
-    @GetMapping(path = "/all")
+    @GetMapping(path = "/students")
     public ResponseEntity<List<Student>> getAllStudents() {
         return this.studentService.findAll();
     }
 
-    @PostMapping(path = "/create")
+    @PostMapping(path = "/students")
     public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         return this.studentService.create(student);
     }
 
-    @GetMapping(path = "/{studentId}")
-    public ResponseEntity<Student> readStudent(@PathParam(value = "studentId") Long id) {
+    @GetMapping(path = "/students/{studentId}")
+    public ResponseEntity<Student> getStudent(@PathParam(value = "studentId") Long id) {
         return this.studentService.findById(id);
     }
 
-    @PutMapping(path = "/{studentId}/update")
+    @PutMapping(path = "/students/{studentId}")
     public ResponseEntity<Student> updateStudent(@PathParam(value = "studentId") Long id, @RequestBody Student student) {
         return this.studentService.update(id, student);
 
     }
     
-    @DeleteMapping(name = "/{studentId}/delete")
+    @DeleteMapping(name = "/students/{studentId}")
     public ResponseEntity<Boolean> deleteStudent(@PathParam(value = "studentId") Long id) {
         this.studentService.delete(id);
         return new ResponseEntity<>(true,HttpStatus.OK);
